283fc6ae0521a196af95d8691eee6eaee1403be8
berk.cosardemir <berk.cosardemir@bordatech.com>
2022-12-06 10:26:38 -0500

    Add Day6 Q1

---
+++ Day6/Q1.cs
@@ -0,0 +1,51 @@
+
+namespace Day6
+{
+    using System.Text;
+    public class Q1 : BaseQuestion
+    {
+        public override object Run()
+        {
+            string input = ReadAllLines()[0];
+            int returnVal = -1;
+            Dictionary<char, int> dict = new();
+            int sequenceStartIndex = 0;
+            dict.Add(input[0], 0);
+
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(input[i]))
+                {
+                    if (dict[input[i]] >= sequenceStartIndex)
+                    {
+                        sequenceStartIndex = dict[input[i]]+1;
+                        dict[input[i]] = i;
+                    }
+                    else
+                    {
+                        if (i + 1 - sequenceStartIndex == 4)
+                        {
+                            returnVal = i+1;
+                            break;
+                        }
+                        dict[input[i]] = i;
+                    }
+                }
+                else
+                {
+                    if (i + 1 - sequenceStartIndex == 4)
+                    {
+                        returnVal = i+1;
+                        break;
+                    }
+                    dict.Add(input[i],i);
+                }
+            }
+
+            return returnVal;
+        }
+
+
+
+    }
+}
